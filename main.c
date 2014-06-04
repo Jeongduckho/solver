550cca44734d0c9bd58b070b5df8b4e6443fb073
Jeongduckho <Jeongduckho@users.noreply.github.com>
2014-06-04 19:57:06 +0900

    Update main.c
    assemble framework

--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 #include "linear_f.h"
 int main() 
 {
-
+  void blank();
+  int t;
+puts("Menu |");
+blank();
+puts("1. 일차함수");
+puts("0. 끝내기")
+blank();
+puts("고르세요.");
+printf(" : ");
+scanf("%d", &t);
+switch(t) {
+  case 1:
+  linear_menu();
+  break;
+  case 0:
+  system("exit");
+  break;
+}
 return 0; 
+}
+void blank() {
+  printf("\n");
 }
