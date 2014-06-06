e950f1231d26774871e7191fef02eca3305fddad
Jeongduckho <duckhoim@gmail.com>
2014-06-06 21:48:07 +0900

    make codes beauty

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include "linear_f.h"
-int main() 
-{
-  void blank();
-  int t;
- puts("Menu |");
- blank();
- puts("1. 일차함수");
- puts("0. 끝내기");
- blank();
- puts("고르세요.");
- printf(" : ");
- scanf("%d", &t);
- switch(t) 
- {
-  case 1: system("clear");
-  linear_menu();
-  break;
-  case 0: system("exit");
-  break; 
- }
- return 0; 
+int main() {
+	void blank();
+	int t;
+	puts("Menu |");
+	blank();
+	puts("1. 일차함수");
+	puts("0. 끝내기");
+	blank();
+	puts("고르세요.");
+	printf(" : ");
+	scanf("%d", & t);
+	switch (t) {
+		case 1:
+			system("clear");
+			linear_menu();
+			break;
+		case 0:
+			system("exit");
+			break;
+	}
+	return 0;
 }
